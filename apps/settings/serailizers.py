969ae171d1b2f3b30bb88febdc762a67db71ef4e
beksultan <nurlanuuulubeksultan@gmail.com>
2025-12-20 17:57:28 +0600

    added filter and pagination, redis

--- apps/settings/serailizers.py
+++ apps/settings/serailizers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 
-from apps.settings.models import Category
+from apps.settings.models import Category, Models
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "title"]
+
+class ModelsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Models
+        fields = ['id', 'title', 'category']
--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.settings.views import CategoryAPIView
+from apps.settings.views import CategoryAPIView, ModelsAPIView
 
 urlpatterns = [
-    path("category-list/", CategoryAPIView.as_view(), name='category-list')
+    path("category-list/", CategoryAPIView.as_view(), name='category-list'), 
+    path("model-list/", ModelsAPIView.as_view(), name='model-list'), 
 ]
